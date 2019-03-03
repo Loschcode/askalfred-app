b848d64e774305e9f96be1ef67bd128df3a0bc68
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-03-03 11:56:10 -0300

    installation of things needed

---
+++ src/components/Any.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="any">
+    Any
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Any',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    <Any />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Any from '@/components/Any.vue'
+
+export default {
+  name: 'index',
+  components: {
+    Any
+  }
+}
+</script>
