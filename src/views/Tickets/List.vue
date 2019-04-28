38ac5f7474650fe1fd9d81a94368b9f3e98a6c7c
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-04-28 13:11:14 -0400

    tickets list design done

--- src/views/Tickets/List.vue
+++ src/views/Tickets/List.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="tickets-list">
-    TICKETS LIST
+    <div class="row center-xs">
+      <div class="col-xs-12">
+        <div class="row center-xs">
+          <div class="col-xs-6">
+            <div class="first-ticket__image">
+              <img src="/images/tickets/rocket-first-ticket.svg">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="first-ticket__text">
+              You don’t have any request yet
+            </div>
+          </div>
+        </div>
+        <div class="row center-xs">
+          <div class="col-xs-10">
+            <div class="first-ticket__button">
+              <div class="button button__blue-on-white">
+                <a href="#">Make your first request</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +37,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.first-ticket__image {
+  padding-top: 4em;
+}
+
+.first-ticket__text {
+  padding-top: 2.5em;
+}
+
+.first-ticket__button {
+  padding-top: 2.5em;
+}
 </style>
