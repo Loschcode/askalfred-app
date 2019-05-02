d68bf9c92995153e29dbec81da13778193905543
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-05-02 12:49:28 -0400

    up block done

--- src/views/Tickets/Show.vue
+++ src/views/Tickets/Show.vue
@@ -35,24 +35,20 @@
 
     <!-- Yourself -->
     <div class="row start-xs">
-      <div class="col-xs-11">
-        <div class="ticket-message">
-          <div class="message message__yourself">
-            <div class="message__yourself-arrow" />
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-          </div>
+      <div class="col-xs-11 ticket-message">
+        <div class="message message__yourself">
+          <div class="message__yourself-arrow" />
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
         </div>
       </div>
     </div>
 
     <!-- Myself -->
     <div class="row end-xs">
-      <div class="col-xs-11">
-        <div class="ticket-message">
-          <div class="message message__myself">
-            <div class="message__myself-arrow" />
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-          </div>
+      <div class="col-xs-11 ticket-message">
+        <div class="message message__myself">
+          <div class="message__myself-arrow" />
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
         </div>
       </div>
     </div>
@@ -88,6 +84,32 @@
           <div class="ticket-notice__symbol">
             <span class="ticket-notice__symbol-border" />
             <span class="ticket-notice__symbol-image"><img src="/images/tickets/icons/add.svg"></span>
+            <span class="ticket-notice__symbol-border" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Yourself (DUPLICATE TO REMOVE) -->
+    <div class="row start-xs">
+      <div class="col-xs-11 ticket-message">
+        <div class="message message__yourself">
+          <div class="message__yourself-arrow" />
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        </div>
+      </div>
+    </div>
+
+    <!--  Notice action done -->
+    <div class="row center-xs">
+      <div class="col-xs-8">
+        <div class="ticket-notice">
+          <div class="ticket-notice__message">
+            Your identity card has been sent successfully
+          </div>
+          <div class="ticket-notice__symbol">
+            <span class="ticket-notice__symbol-border" />
+            <span class="ticket-notice__symbol-image"><img src="/images/tickets/icons/up.svg"></span>
             <span class="ticket-notice__symbol-border" />
           </div>
         </div>
