760a57ad572136aee90288504a9ece258c7737a5
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-04 15:32:48 -0400

    connect history attempt with express

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
 
-let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+let app = express()
+app.use(serveStatic(__dirname + '/dist'))
+app.use(history())
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log('Listening on port ' + port)
-});
+})
