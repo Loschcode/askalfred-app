c4178acca790947dac76d67972c60cdc62ea1d52
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-04 15:54:26 -0400

    rewriting for server serving

--- server.js
+++ server.js
@@ -12,18 +12,42 @@
 //   console.log('Listening on port ' + port)
 // })
 
-// server.js
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-// added this middleware
-var history = require('connect-history-api-fallback')
+// // server.js
+// var express = require('express')
+// var path = require('path')
+// var serveStatic = require('serve-static')
+// // added this middleware
+// var history = require('connect-history-api-fallback')
 
-app = express()
-// started middleware
-app.use(history())
+// app = express()
+// // started middleware
+// app.use(history())
 
-app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+// app.use(serveStatic(__dirname))
+// var port = process.env.PORT || 5000
+// app.listen(port)
+// console.log('server started ' + port)
+
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+
+const app = express()
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+
+app.use(staticFileMiddleware)
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}))
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'))
+})
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port
+  console.log('App now running on port', port)
+})
